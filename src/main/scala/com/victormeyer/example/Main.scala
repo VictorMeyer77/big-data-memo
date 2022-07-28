f12b9dab201135a38f18bebe2e6ae2205ebf57e8
Victor-Algo <victor.algo.contact@gmail.com>
2022-07-28 21:48:15 +0200

    test Main.scala

--- src/main/scala/com/victormeyer/example/Main.scala
+++ src/main/scala/com/victormeyer/example/Main.scala
@@ -1,5 +1,13 @@
 package com.victormeyer.example
 
+import com.victormeyer.example.streaming.stockExample.DataGenerator
+
 object Main {
+
+  def main(args: Array[String]): Unit ={
+
+    DataGenerator.createDataframe.show()
+
+  }
 
 }
