03eea9a2a99129ccfcbfa734ec802b8e282f3aa5
Victor-Algo <victor.algo.contact@gmail.com>
2022-10-25 23:01:42 +0200

    add tests

---
+++ src/test/scala/com/victormeyer/example/SandboxTest.scala
@@ -0,0 +1,18 @@
+package com.victormeyer.example
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class SandboxTest extends AnyFlatSpec {
+
+  "test.test" should "test" in {
+
+    // given
+
+    // when
+
+    // then
+    assert(1 == 1)
+
+  }
+
+}
